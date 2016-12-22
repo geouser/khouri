49570aff6348a6b428de1e98b6d2e2fc97a28bfd
rusyi <olehrusy@gmail.com>
2016-12-22 17:19:59 +0200

    s

--- js/main.js
+++ js/main.js
@@ -65,6 +65,39 @@ jQuery(document).ready(function($) {
         speed: 700
     });
 
+    $('.team__slider').slick({
+        speed: 700,
+        arrows: true,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
+
     /*----------------------------
                               SEND FORM
     -------------------------*/
